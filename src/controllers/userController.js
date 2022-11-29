98baebbc8e707c950312760959435a00ec0bac84
whdnd <whdnd1225@naver.com>
2022-11-29 17:00:40 +0900

    make kakao login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User';
+import fetch from 'node-fetch';
 import bcrypt from 'bcrypt';
 
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
@@ -45,7 +46,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = 'Login';
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render('login', {
       pageTitle,
@@ -65,7 +66,179 @@ export const postLogin = async (req, res) => {
   req.session.user = user;
   return res.redirect('/');
 };
+
+export const startGithunLogin = (req, res) => {
+  const baseUrl = 'https://github.com/login/oauth/authorize';
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: 'read:user user:email',
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = 'https://github.com/login/oauth/access_token';
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+  ).json();
+
+  if ('access_token' in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = 'https://api.github.com';
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+
+    if (!emailObj) {
+      return res.redirect('/login');
+    }
+
+    let user = await User.findOne({ email: emailObj.email });
+
+    if (!user) {
+      user = await User.create({
+        name: userData.name,
+        avatarUrl: userData.avatar_url,
+        username: userData.login,
+        email: emailObj.email,
+        password: '',
+        socialOnly: true,
+        location: userData.location,
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect('/');
+  } else {
+    return res.redirect('/login');
+  }
+};
+
+export const startKakaoLogin = (req, res) => {
+  const baseUrl = 'https://kauth.kakao.com/oauth/authorize';
+  const config = {
+    client_id: process.env.KAKAO_CLIENT,
+    redirect_uri: 'http://localhost:4000/users/kakao/finish',
+    response_type: 'code',
+    scope: 'profile_nickname,profile_image,account_email',
+  };
+
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishKakaoLogin = async (req, res) => {
+  const baseUrl = 'https://kauth.kakao.com/oauth/token';
+  const config = {
+    grant_type: 'authorization_code',
+    client_id: process.env.KAKAO_REST_API,
+    redirect_uri: 'http://localhost:4000/users/kakao/finish',
+    code: req.query.code,
+  };
+
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+  ).json();
+
+  if ('access_token' in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = 'https://kapi.kakao.com/v2/user/me';
+
+    const userData = await (
+      await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+
+    const emailData = [
+      await (
+        await fetch(`${apiUrl}?property_keys=["kakao_account.email"]`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${access_token}`,
+          },
+        })
+      ).json(),
+    ];
+
+    const emailObj = emailData.find((email) => {
+      const account = email.kakao_account;
+      return (
+        account.is_email_valid === true && account.is_email_verified === true
+      );
+    });
+
+    if (!emailObj) {
+      return res.redirect('/login');
+    }
+
+    let user = await User.findOne({ email: emailObj.kakao_account.email });
+
+    if (!user) {
+      const userProfile = userData.kakao_account.profile;
+      user = await User.create({
+        name: userProfile.nickname,
+        avatarUrl: userProfile.profile_image_url,
+        username: userProfile.nickname,
+        email: emailObj.kakao_account.email,
+        password: '',
+        socialOnly: true,
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect('/');
+  } else {
+    return res.redirect('/login');
+  }
+};
+
 export const edit = (req, res) => res.send('Edit User');
 export const remove = (req, res) => res.send('Remove User');
-export const logout = (req, res) => res.send('Logout');
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect('/');
+};
 export const see = (req, res) => res.send('see');
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,25 @@
 import express from 'express';
-import { edit, remove, logout, see } from '../controllers/userController';
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithunLogin,
+  finishGithubLogin,
+  startKakaoLogin,
+  finishKakaoLogin,
+} from '../controllers/userController';
 
 const userRouter = express.Router();
 
 userRouter.get('/logout', logout);
 userRouter.get('/edit', edit);
 userRouter.get('/remove', remove);
+userRouter.get('/github/start', startGithunLogin);
+userRouter.get('/github/finish', finishGithubLogin);
+userRouter.get('/kakao/start', startKakaoLogin);
+userRouter.get('/kakao/finish', finishKakaoLogin);
+
 userRouter.get(':id', see);
 
 export default userRouter;
